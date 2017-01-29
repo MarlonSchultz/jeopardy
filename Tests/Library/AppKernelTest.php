babc2e5936ab3a332468546ea495b75ed0ae3782
Marlon Schultz <spam@marlonschultz.de>
2017-01-29 14:38:35 +0100

    added test for appkernel

---
+++ Tests/Library/AppKernelTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mgbs
+ * Date: 29.01.17
+ * Time: 12:44
+ */
+
+namespace mgbs\Library;
+
+
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Routing\RouteCollection;
+
+class AppKernelTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIfKernelReturnsHttpKernelInterface()
+    {
+        $routeCollection = new RouteCollection();
+        $appKernel = new AppKernel($routeCollection);
+        self::assertTrue(is_a($appKernel->getKernel(), HttpKernel::class));
+        self::assertInstanceOf(HttpKernelInterface::class, $appKernel->getKernel());
+    }
+}
