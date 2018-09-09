34d883807426a56aea5e7bc57c59eb3628eef0db
Marlon Schultz <marlon.schultz@mayflower.de>
2018-09-09 18:09:37 +0200

    restructed directories

---
+++ Code/PHP/Controller/RestController.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace mgbs\Controller;
+
+use mgbs\Library\DITrait;
+use mgbs\Model\PlayerAnswerModel;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Created by PhpStorm.
+ * User: mgbs
+ * Date: 16.01.17
+ * Time: 19:37
+ */
+class RestController
+{
+    use DITrait;
+
+    /**
+     * @var PlayerAnswerModel
+     */
+    private $playeranswermodel;
+
+    public function __construct()
+    {
+        $this->playeranswermodel = $this->getService('playeranswermodel');
+    }
+
+    /**
+     * @return Response
+     */
+    public function buzzerAction($buzzer): Response
+    {
+        $this->playeranswermodel->insertBuzz((int) $buzzer);
+        return new Response('KotRoller aufgerufen . ');
+    }
+
+
+}
