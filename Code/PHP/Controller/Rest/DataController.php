702ce3c2a7fdae0ee5b141b83ca8331597c88d4f
Marlon Schultz <marlon.schultz@mayflower.de>
2018-09-15 15:25:17 +0200

    added base controller for simple rest request to acquire all answers

--- Code/PHP/Controller/Rest/DataController.php
+++ Code/PHP/Controller/Rest/DataController.php
@@ -5,19 +5,29 @@ namespace mgbs\Controller\Rest;
 
 
 use mgbs\Library\DITrait;
+use mgbs\Model\QuestionsModel;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DataController
 {
     use DITrait;
 
+    /**
+     * @var QuestionsModel
+     */
+    private $questionModel;
+
     public function __construct()
     {
-        
+        $this->questionModel = $this->getService('questionmodel');
     }
-    public function getAnswers()
-        
-    {
 
+    public function getAnswersAction(): JsonResponse
+    {
+        if (!\is_array($data = $this->questionModel->getAllQuestions())) {
+            throw new \DatabaseException('Seems DB is empty');
+        }
+        return new JsonResponse($data);
     }
 
 
---
+++ Code/PHP/Exceptions/DatabaseException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mgbs
+ * Date: 15.09.18
+ * Time: 15:21
+ */
+declare(strict_types=1);
+
+class DatabaseException extends Exception
+{
+
+}
