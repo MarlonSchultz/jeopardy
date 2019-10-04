1f06f89265bfb2b4c09b23e87c5361afb5b5b32f
Marlon <spam@marlonschultz.de>
2019-10-04 22:28:36 +0200

    Websocket server (#21)
    * added baseline for websockets
    
    * websocket server is working with test message
    
    * fixed port in websocket

---
+++ Code/NodeJS/server.js
@@ -0,0 +1,38 @@
+let WebSocketServer = require('websocket').server;
+let http = require('http');
+let server = http.createServer(function (request, response) {
+
+});
+server.listen(process.env.PORT, function () {
+});
+
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function (request) {
+    let connection = request.accept(null, request.origin);
+
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function (message) {
+        if (message.type === 'utf8') {
+            // process WebSocket message
+            connection.sendUTF("test")
+        }
+    });
+
+    connection.on('close', function (connection) {
+        // close user connection
+    });
+});
+
+// kick everyone off the server if docker shuts down
+process.on('SIGINT', function () {
+    // terminate all clients on close
+    wsServer.closeAllConnections();
+    console.log('Good bye');
+    process.exit();
+});
