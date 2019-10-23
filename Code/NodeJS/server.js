bd5dbe97ba775deec5557849ce645d18ad202fd6
Marlon Schultz <spam@marlonschultz.de>
2019-10-23 15:29:30 +0200

    added message case to node, node writes to file; curently ignoreing node in docker-compose for local development

--- Code/NodeJS/server.js
+++ Code/NodeJS/server.js
@@ -1,5 +1,6 @@
 let WebSocketServer = require('websocket').server;
 let http = require('http');
+const fs = require("fs");
 let server = http.createServer(function (request, response) {
 
 });
@@ -18,9 +19,34 @@ wsServer.on('request', function (request) {
     // This is the most important callback for us, we'll handle
     // all messages from users here.
     connection.on('message', function (message) {
-        if (message.type === 'utf8') {
-            // process WebSocket message
-            connection.sendUTF("test")
+        try {
+            if (message.type === 'utf8') {
+                // QuestionOpened
+                let payload = JSON.parse(message.utf8Data);
+
+                if (payload.message === 'questionOpen') {
+                    fs.writeFile('questionOpen', 'True', (err) => {
+                        // throws an error, you could also catch it here
+                        if (err) throw err;
+
+                        // success case, the file was saved
+                        console.log('questionOpen!');
+                    });
+                }
+
+                // QuestionClosed
+                if (payload.message === 'questionClosed') {
+                    fs.writeFile('questionOpen', 'False', (err) => {
+                        // throws an error, you could also catch it here
+                        if (err) throw err;
+
+                        // success case, the file was saved
+                        console.log('questionClosed!');
+                    });
+                }
+            }
+        } catch (e) {
+            console.log('Error:', e);
         }
     });
 
