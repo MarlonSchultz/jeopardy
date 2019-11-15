aff9674a415dadf64de3f46dd1bcb5457544c163
Marlon Schultz <spam@marlonschultz.de>
2019-11-15 08:25:59 +0100

    github actions works for yarn; added ci env var to package.json to skip server start

--- Code/NodeJS/server.js
+++ Code/NodeJS/server.js
@@ -66,65 +66,66 @@ module.exports = {
 closeQuestion();
 
 //create a server object:
-http.createServer(function (request, response) {
-    try {
-        switch (request.url.toLowerCase()) {
-            case '/openquestion':
-                openQuestion();
-                response.write('Question opened');
-                response.end();
-                break;
+if (process.env.CI !== 'CI') {
+    http.createServer(function (request, response) {
+        try {
+            switch (request.url.toLowerCase()) {
+                case '/openquestion':
+                    openQuestion();
+                    response.write('Question opened');
+                    response.end();
+                    break;
 
-            case '/closequestion':
-                closeQuestion();
-                response.write('Question closed');
-                response.end();
-                break;
+                case '/closequestion':
+                    closeQuestion();
+                    response.write('Question closed');
+                    response.end();
+                    break;
 
-            case '/getbuzzer':
-                response.write(getBuzzer());
-                response.end();
-                break;
+                case '/getbuzzer':
+                    response.write(getBuzzer());
+                    response.end();
+                    break;
 
-            case '/setbuzzer/green':
-                setBuzzer('green');
-                response.write('Buzzed green');
-                response.end();
-                break;
+                case '/setbuzzer/green':
+                    setBuzzer('green');
+                    response.write('Buzzed green');
+                    response.end();
+                    break;
 
-            case '/setbuzzer/blue':
-                setBuzzer('blue');
-                response.write('Buzzed blue');
-                response.end();
-                break;
+                case '/setbuzzer/blue':
+                    setBuzzer('blue');
+                    response.write('Buzzed blue');
+                    response.end();
+                    break;
 
-            case '/setbuzzer/yellow':
-                setBuzzer('yellow');
-                response.write('Buzzed yellow');
-                response.end();
-                break;
+                case '/setbuzzer/yellow':
+                    setBuzzer('yellow');
+                    response.write('Buzzed yellow');
+                    response.end();
+                    break;
 
-            case '/setbuzzer/red':
-                setBuzzer('red');
-                response.write('Buzzed red');
-                response.end();
-                break;
+                case '/setbuzzer/red':
+                    setBuzzer('red');
+                    response.write('Buzzed red');
+                    response.end();
+                    break;
 
-            case '/setbuzzer/none':
-                setBuzzer('none');
-                response.write('Reset buzzer');
-                response.end();
-                break;
+                case '/setbuzzer/none':
+                    setBuzzer('none');
+                    response.write('Reset buzzer');
+                    response.end();
+                    break;
 
-            default:
-                console.log('No url pattern matched');
-                response.end();
-                break;
+                default:
+                    console.log('No url pattern matched');
+                    response.end();
+                    break;
+            }
+
+        } catch (e) {
+            console.log('Weird things! Error:', e);
         }
 
-    } catch (e) {
-        console.log('Weird things! Error:', e);
-    }
-
-}).listen(process.env.PORT); //the server object listens on port 8080
-
+    }).listen(process.env.PORT); //the server object listens on port 8080
+}
